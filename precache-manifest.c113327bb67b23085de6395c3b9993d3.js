38fd52d8fe6229dcf0d4553a49ca552242a91b60
epictete <john.laterre@gmail.com>
2020-09-26 21:19:04 +0200

    Updates

---
+++ precache-manifest.c113327bb67b23085de6395c3b9993d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e74e5d5ca0eb7ab72b19d9f84e978ea",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "0fe359e9e9de2fa84d3e",
+    "url": "/react-todo/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "7fb54f1486025a363fa1",
+    "url": "/react-todo/static/js/2.84173d03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo/static/js/2.84173d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe359e9e9de2fa84d3e",
+    "url": "/react-todo/static/js/main.b6fc798b.chunk.js"
+  },
+  {
+    "revision": "2c65fcd9f186fe64c1c3",
+    "url": "/react-todo/static/js/runtime-main.f4208ecd.js"
+  }
+]);
